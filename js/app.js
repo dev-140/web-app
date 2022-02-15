ff9a8b26976041104c170214522548152499554b
rox <roxmarzan14@gmail.com>
2022-02-15 19:35:37 +0800

    new features

--- js/app.js
+++ js/app.js
@@ -26,4 +26,33 @@ $(document).ready(function(){
         $('.hamburger-menu-container').removeClass('active');
         $('.hamburger-menu-container').addClass('close');
     });
+
+    $('.add-expense-container .add-category-button, .add-expense-container .categories p').on('click', (e)=> {
+        $('.add-expense-container .categories').toggleClass('active');
+        $('.add-category-button .arrow-down-category').toggleClass('active');
+    });
+
+    $('.add-expense-container .categories p').on('click', (e)=> {
+        var items = $(e.currentTarget).text();
+        $('.add-category-button .text').text(items);
+    });
+
+    $('.add-expense-data').on('click', (e) => {
+        setTimeout(() => { 
+            $('.add-expense-value').val('');
+            $('.add-category-button .text').text('Category');
+        }, 300);
+    });
+
+    setInterval(function () {
+        var category = $('.add-category-button .text').text(),
+            value = $('.add-expense-value').val();
+        if (category === 'Category'){
+            $('.add-expense-data').hide();
+        } else if (value === ''){
+            $('.add-expense-data').hide();
+        } else {
+            $('.add-expense-data').show();
+        }
+    },);
 });
